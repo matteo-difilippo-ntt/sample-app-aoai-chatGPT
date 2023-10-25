949b8d6a9df615e8d64b72ebe8a6e4c17adb8416
matteo-df <matteodifilippo22@gmail.com>
2023-10-25 15:58:20 +0200

    add View Source button

--- frontend/src/pages/chat/Chat.tsx
+++ frontend/src/pages/chat/Chat.tsx
@@ -674,6 +674,17 @@ const Chat = () => {
                     <Stack.Item className={styles.citationPanel} tabIndex={0} role="tabpanel" aria-label="Citations Panel">
                         <Stack aria-label="Citations Panel Header Container" horizontal className={styles.citationPanelHeaderContainer} horizontalAlign="space-between" verticalAlign="center">
                             <span aria-label="Citations" className={styles.citationPanelHeader}>Citations</span>
+                            <span 
+                                title={activeCitation.url ? activeCitation.url : ""} 
+                                tabIndex={0} 
+                                role="link" 
+                                onClick={() => onViewSource(activeCitation)} 
+                                onKeyDown={e => e.key === "Enter" || e.key === " " ? onViewSource(activeCitation) : null}
+                                className={styles.viewSourceButton}
+                                // aria-label={activeCitation.url}
+                            >
+                                View Source
+                            </span>
                             <IconButton iconProps={{ iconName: 'Cancel'}} aria-label="Close citations panel" onClick={() => setIsCitationPanelOpen(false)}/>
                         </Stack>
                         <h5 
@@ -684,19 +695,6 @@ const Chat = () => {
                         >
                         {activeCitation.title}
                         </h5>
-                        { (activeCitation.url) &&
-                            <span 
-                                title={activeCitation.url ? activeCitation.url : ""} 
-                                tabIndex={0} 
-                                role="link" 
-                                onClick={() => onViewSource(activeCitation)} 
-                                onKeyDown={e => e.key === "Enter" || e.key === " " ? onViewSource(activeCitation) : null}
-                                className={styles.viewSourceButton}
-                                aria-label={activeCitation.url}
-                            >
-                                View Source
-                            </span>
-                        }
                         <div tabIndex={0}> 
                         <ReactMarkdown 
                             linkTarget="_blank"
